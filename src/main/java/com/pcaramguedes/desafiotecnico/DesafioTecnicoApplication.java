dd391e17204cea52caceba25154bc65adeaac360
pcaramguedes <pcaramguedes@gmail.com>
2021-03-30 18:50:17 -0300

    Alterado o nome do main principal

--- src/main/java/com/pcaramguedes/desafiotecnico/DesafioTecnicoApplication.java
+++ src/main/java/com/pcaramguedes/desafiotecnico/DesafioTecnicoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesatiotecnicoApplication {
+public class DesafioTecnicoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesatiotecnicoApplication.class, args);
+		SpringApplication.run(DesafioTecnicoApplication.class, args);
 	}
 
 }
--- src/main/java/com/pcaramguedes/desafiotecnico/controller/InicioController.java
+++ src/main/java/com/pcaramguedes/desafiotecnico/controller/InicioController.java
@@ -1,4 +1,4 @@
-package com.pcaramguedes.desafiotecnico.Controller;
+package com.pcaramguedes.desafiotecnico.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
